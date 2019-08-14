a5baa062a268abc477cacf45dc5833566835c0f1
gh05d <pc@vipfy.com>
2019-08-14 19:50:33 +0200

    Major Revamp of the Generator

--- server.js
+++ server.js
@@ -1,50 +1,61 @@
-const express = require('express');
-const hbs = require('hbs');
-const fs = require('fs');
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 // Needed to deploy the app to heroku. Uses localhost 3000 in machine.
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials');
-app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
 
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
   // Create the log file.
-  console.log(log);
-  fs.appendFile('server.log', log + '\n', err => {
-    if(err) {
-      console.log('Unable to append to server.log');
+
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("Unable to append to server.log");
     }
   });
   next();
 });
 
-// app.use((req, res, next) => {
-//   res.render('maintenance.hbs');
-// });
+app.use(express.static(__dirname + "/public"));
 
-app.use(express.static(__dirname + '/public'));
+hbs.registerHelper("getCurrentYear", () => new Date().getFullYear());
 
-hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
-});
-
-hbs.registerHelper('getTime', () => {
+hbs.registerHelper("getTime", () => {
   let hours = new Date().getHours();
   let gmin = new Date().getMinutes();
-  let minutes = (gmin < 10 ? '0' : '') + gmin;
+  let minutes = (gmin < 10 ? "0" : "") + gmin;
 
   return `${hours}:${minutes}`;
 });
 
-app.get('/', (req, res) => {
-  res.render('home.hbs', {
-    pageTitle: 'Doppelkinn-Domme'
+app.get("/", (_req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Doppelkinn-Domme"
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}...`);
+app.get("/neue-ausrede", (req, res) => {
+  [from, message] = req.url.split("?")[1].split("&");
+
+  const msg = {
+    to: "pc@vipfy.store",
+    from,
+    subject: "Neue Ausrede für den Generator",
+    text: decodeURIComponent(message)
+  };
+
+  sgMail.send(msg);
+  res.render("home.hbs", {
+    pageTitle: "Doppelkinn-Domme"
+  });
 });
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
